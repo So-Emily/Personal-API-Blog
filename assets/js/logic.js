82993d24eb98f2d681347427af9524b1cb7ea060
Emily Soriano <esoriano110@gmail.com>
2024-06-24 18:32:41 -0700

    getting that dark mode button looking good

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,43 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     const body = document.querySelector('body');
-    const icon = document.querySelector('.icon-selector');
     const formTitle = document.querySelector('.form-title');
     const formLabels = document.querySelectorAll('.form-label');
 
-    if (!body || !icon || !formTitle || !formLabels.length) {
+    if (!body || !formTitle || !formLabels.length) {
         console.error('One or more elements are missing!');
         return;
     }
 
-    document.getElementById('modeToggle').addEventListener('click', function() {
-        const body = document.body;
-        const icon = this.querySelector('i');
+    const modeToggle = document.getElementById('modeToggle');
+    const darkModeIcon = modeToggle.querySelector('.dark-mode');
 
-        if (icon) {
-            icon.classList.replace('fa-moon', 'fa-sun');
-        }
-
+    modeToggle.addEventListener('click', function() {
+        // Toggle dark mode
         if (body.classList.contains('bg-dark')) {
             body.classList.replace('bg-dark', 'bg-light');
             body.classList.replace('text-light', 'text-dark');
-            this.textContent = 'Toggle Light Mode';
-
             formTitle.classList.replace('text-light', 'text-dark');
             formLabels.forEach(label => label.classList.replace('text-light', 'text-dark'));
+            
+            //darkModeIcon.style.display = 'none';
+        // Toggle light mode
         } else {
             body.classList.replace('bg-light', 'bg-dark');
             body.classList.replace('text-dark', 'text-light');
-            this.textContent = 'Toggle Dark Mode';
-
             formTitle.classList.replace('text-dark', 'text-light');
             formLabels.forEach(label => label.classList.replace('text-dark', 'text-light'));
-        }
-
-        if (body.classList.contains('bg-dark')) {
-            this.style.backgroundColor = '#333';
             
-        } else {
-            this.style.backgroundColor = '#FFF';
+            // darkModeIcon.style.display = 'none';
         }
     });
 });
