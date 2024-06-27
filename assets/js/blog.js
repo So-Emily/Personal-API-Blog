a368ac4602f041b3c5bf497e0bae3244fb2fdd33
Emily Soriano <esoriano110@gmail.com>
2024-06-26 17:41:38 -0700

    now have a back button and a button to go to the blog on the form page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 
     posts.forEach(function(post) {
         const postElement = `
-            <div class="card bg-light text-dark">
+            <div class="card bg-light text-dark form-title">
                 <div class="card-header">
-                    <h3 class="text-decoration-underline">${post.title}</h3>
+                    <h3 class="card-title text-light form-label">${post.title}</h3>
                 </div>
 
-                <p class="card-content bg-light text-dark">${post.content}</p>
+                <p class="card-content bg-light text-dark form-label">${post.content}</p>
 
-                <p class="card-author bg-light text-dark">- ${post.username}</p>
+                <p class="card-author bg-light text-dark form-label">xoxo, ${post.username}</p>
             </div>
         `;
         $('#blog-container').append(postElement);
